dcfe3fbd1d5fbd9d305c7a55cbd1fbd10fbf5e64
Thomas Barrett <tbarrett1200@icloud.com>
2023-02-19 21:45:31 +0000

    Configure devcontainer to use privledged container

--- main/main.c
+++ main/main.c
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <sched.h>
+#include <errno.h>
 
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -19,18 +20,21 @@ int jail(void *args) {
 int main(int argc, char const *argv[]) {
     char *stack = (char *) mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
     if (stack == MAP_FAILED) {
+        perror("mmap failed");
         exit(EXIT_FAILURE);
     }
     char *stack_top = stack + STACK_SIZE;
 
-    printf("Hello, World! ( parent ) \n");
+    printf("Hello, World! ( foo ) \n");
 
-    int pid = clone(jail, stack_top, SIGCHLD, 0);
+    int pid = clone(jail, stack_top, CLONE_NEWUTS | SIGCHLD, 0);
     if (pid == -1) {
+        perror("clone failed");
         exit(EXIT_FAILURE);
     }
 
     if (waitpid(pid, NULL, 0) == -1)  {
+        perror("waitpid failed");
         exit(EXIT_FAILURE);
     }
     
@@ -38,3 +42,4 @@ int main(int argc, char const *argv[]) {
 
     return EXIT_SUCCESS;
 }
+
